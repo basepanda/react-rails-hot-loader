02b4e93db7605242ab396bcaae3c77a3bbc77f83
Yohan Robert <yohan.robert@outlook.fr>
2017-12-27 16:37:31 +0100

    Make asset path configurable

--- lib/hot_loader/asset_change_set.rb
+++ lib/hot_loader/asset_change_set.rb
@@ -3,20 +3,22 @@ module React
     module HotLoader
       class AssetChangeSet
         attr_reader :since, :path, :changed_files, :changed_file_names
-        class_attribute :asset_glob, :bankruptcy_count
+        class_attribute :asset_glob, :asset_path, :bankruptcy_count
         # Search for changes with this glob
         self.asset_glob = "/**/*.{css,sass,scss,js,coffee}*"
+        self.asset_path = "app/assets"
         # If this many files change at once, give up hope! (Probably checked out a new branch)
         self.bankruptcy_count = 5
 
         # initialize with a path and time
         # to find files which changed since that time
-        def initialize(since:, path: ::Rails.root.join("app/assets"))
+        def initialize(since:, path: ::Rails.root.join(self.class.asset_path))
           @since = since
           @path = path.to_s
           asset_glob = File.join(path, AssetChangeSet.asset_glob)
-          @changed_files = Dir.glob(asset_glob).select { |f| File.mtime(f) >= since }.uniq
-          @changed_file_names = changed_files.map { |f| File.basename(f) }
+          @changed_files = Dir.glob(asset_glob).uniq
+          @changes = Dir.glob(asset_glob).select { |f| File.mtime(f) >= since }.any?
+          @changed_file_names = Dir.glob(asset_glob).select { |f| File.basename(f) == "components.js" }
         end
 
         def bankrupt?
@@ -24,7 +26,7 @@ module React
         end
 
         def any?
-          changed_files.any?
+          @changes
         end
 
         def as_json(options={})
@@ -37,7 +39,7 @@ module React
         end
 
         def changed_asset_contents
-          @changed_asset_contents ||= changed_files.map do |f|
+          @changed_asset_contents ||= changed_file_names.map do |f|
             logical_path = to_logical_path(f)
             asset = ::Rails.application.assets[logical_path]
             asset.to_s
